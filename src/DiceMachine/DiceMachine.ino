6b88c64739671cd8f8224a0d01d122f65f3270fe
Indrek Luuk <indrek.luuk@gmail.com>
2022-08-13 02:25:58 +0300

    Updated dice to show statistics if button held down for 3 seconds.

--- src/DiceMachine/DiceMachine.ino
+++ src/DiceMachine/DiceMachine.ino
@@ -7,7 +7,6 @@
 #include <Adafruit_PCD8544.h>
 
 
-
 int8_t BUTTON_PIN = 12;
 
 int8_t SCREEN_BACK_LIGHT = 8;
@@ -20,7 +19,6 @@ int8_t SCREEN_PIN_RST = 4;
 Adafruit_PCD8544 display = Adafruit_PCD8544(SCREEN_PIN_SCLK, SCREEN_PIN_DIN, SCREEN_PIN_DC, SCREEN_PIN_RST);
 
 
-
 // display: 84x48. non-square pixels (ratio ~1.2)
 static const uint16_t SCREEN_WIDTH = 84;
 static const uint16_t SCREEN_HEIGHT = 48;
@@ -30,8 +28,9 @@ static const uint16_t DICE_CORNER = 5;
 static const uint16_t DOT_WIDTH = 6;
 static const uint16_t DOT_HEIGHT = 5;
 
-static const uint16_t LIGHT_ON_milliseconds = 3000;
-
+static const uint16_t LIGHT_ON_milliseconds = 10000;
+static const uint16_t ROLL_TIMEOUT_milliseconds = 500;
+static const uint16_t STAT_milliseconds = 3000;
 
 
 class Die {
@@ -88,8 +87,6 @@ private:
     }
   }
   
-  
-  
   void drawFace(uint8_t number) {
     
     int8_t dW = m_w/2;
@@ -137,7 +134,6 @@ private:
     }
   }
   
-  
   void drawDot(int8_t dX, int8_t dY) {
     uint8_t displaceX = DOT_WIDTH/2;
     uint8_t displaceY = DOT_HEIGHT/2;
@@ -149,51 +145,26 @@ private:
       DOT_HEIGHT,
       BLACK);
   }
-  
-  
-
-  
 };
 
 
 
 
 
-Die die_1(1, (SCREEN_HEIGHT-DICE_HEIGHT)/2, DICE_WIDTH, DICE_HEIGHT);
-Die die_2(SCREEN_WIDTH - 2 - DICE_WIDTH, (SCREEN_HEIGHT-DICE_HEIGHT)/2, DICE_WIDTH, DICE_HEIGHT);
+Die die_1(1, (SCREEN_HEIGHT-DICE_HEIGHT)/2 - 4, DICE_WIDTH, DICE_HEIGHT);
+Die die_2(SCREEN_WIDTH - 2 - DICE_WIDTH, (SCREEN_HEIGHT-DICE_HEIGHT)/2 - 4, DICE_WIDTH, DICE_HEIGHT);
 
+uint8_t rolledDie1Number = 0;
+uint8_t rolledDie2Number = 0;
 
+long buttonPressedTimer = 0;
+long buttonReleaseTimer = 0;
+long backLightOnTimer = 0;
 
-
-
-void initRandomSeed() {
-  randomSeed(millis() + random());
-}
-
-
-void roll() {
-  display.clearDisplay();
-  die_1.draw(random(6)+1);
-  die_2.draw(random(6)+1);
-  display.display();
-}
-
-
-
-uint32_t backLightOnTimestamp = 0;
-
-
-void backLightOn() {
-  backLightOnTimestamp = millis();
-  digitalWrite(SCREEN_BACK_LIGHT, HIGH);
-}
-
-void checkBackLighthStatus() {
-  if (backLightOnTimestamp + LIGHT_ON_milliseconds < millis()) {
-    digitalWrite(SCREEN_BACK_LIGHT, LOW);
-  }  
-}
-
+uint16_t rollCount = 0;
+uint16_t die1Stat[6] = {0,0,0,0,0,0};
+uint16_t die2Stat[6] = {0,0,0,0,0,0};
+uint16_t diceSum[11] = {0,0,0,0,0,0,0,0,0,0,0};
 
 
 
@@ -207,44 +178,152 @@ void setup() {
   digitalWrite(SCREEN_BACK_LIGHT, LOW);
 
   initRandomSeed();
-
-  roll();
+  drawDice(roll(), roll(), true);
 }
 
-
-
-
-bool isPreviousButtonStatusPressed = false;
 
 
 void loop() {
-  bool isCurrentButtonStatusPressed = !digitalRead(BUTTON_PIN);
 
-  if (isCurrentButtonStatusPressed) {
-    if (!isPreviousButtonStatusPressed) {
+  if (isButtonDown()) {
+    if (!buttonPressedTimer) {
       initRandomSeed();
+      // Decided the roll after first press so drawing doesn't affect the result.
+      rolledDie1Number = roll();
+      rolledDie2Number = roll();
+      buttonPressedTimer = millis();
     }
-    roll();    
-    backLightOn();
-    delay(50);
-  } else {
-    if (isPreviousButtonStatusPressed) {
-      roll(); // one last roll when button is released
-    }
+    buttonReleaseTimer = millis();
   }
 
-  isPreviousButtonStatusPressed = isCurrentButtonStatusPressed;
+  if (buttonPressedTimer) {
+    bool showStat = (millis() - buttonPressedTimer) > STAT_milliseconds;
+
+    if (showStat) {
+      displayStat();
+    } else {
+      backLightOn();
+      drawDice(roll(), roll(), false);
+      delay(50);     
+    }
+
+    if ((millis() - buttonReleaseTimer) > ROLL_TIMEOUT_milliseconds) {
+      buttonPressedTimer = 0;
+      if (!showStat) {
+        recordStat(rolledDie1Number, rolledDie2Number);
+        drawDice(rolledDie1Number, rolledDie2Number, true);        
+      }
+    }    
+  }
+  
   checkBackLighthStatus();
+}
+
+bool isButtonDown() {
+  return !digitalRead(BUTTON_PIN);
 }
 
 
 
 
 
+void initRandomSeed() {
+  randomSeed(millis() + random());
+}
+
+
+uint8_t roll() {
+  return random(6)+1;
+}
+
+void recordStat(uint8_t die1Number, uint8_t die2Number) {
+  rollCount++;
+  die1Stat[die1Number - 1]++;
+  die2Stat[die2Number - 1]++;
+  diceSum[(die1Number - 1) + (die2Number - 1)]++;
+}
+
+
+void drawDice(uint8_t die1Number, uint8_t die2Number, bool showSum) {
+  display.clearDisplay();
+  die_1.draw(die1Number);
+  die_2.draw(die2Number);
+  if (showSum) {
+    uint8_t sum = die1Number + die2Number;
+    display.setCursor(40 - (sum > 9 ? 3 : 0), 41);
+    display.print(sum);    
+  }
+  display.display();
+}
+
+
+void displayStat() {
+  display.clearDisplay();
+  
+  display.setRotation(1);
+  display.setTextColor(WHITE, BLACK);  
+  display.fillRect(0, 0, 48, 9, BLACK);
+  display.setCursor(2, 1);
+  display.print(rollCount);
+  
+  display.setRotation(0);
+  display.setTextColor(BLACK, WHITE);
+  /*
+  die1Stat[0] = 100;
+  die2Stat[0] = 70;
+  diceSum[0] = 150;
+  diceSum[10] = 150;
+  */
+  
+
+  uint8_t graphH = display.height() / 2;
+
+  uint16_t die1MaxRolls = getMaxRolls(die1Stat, 6);
+  uint16_t die2MaxRolls = getMaxRolls(die2Stat, 6);
+  uint16_t diceMaxRolls = die1MaxRolls > die2MaxRolls ? die1MaxRolls : die2MaxRolls;
+  drawGraph(die1Stat, 6, 20, graphH - 1, diceMaxRolls, graphH - 1);
+  drawGraph(die2Stat, 6, 52, graphH - 1, diceMaxRolls, graphH - 1);
+
+  uint16_t sumMaxRolls = getMaxRolls(diceSum, 11);
+  drawGraph(diceSum, 11, 26, graphH*2, sumMaxRolls, graphH - 1);
+  
+  display.display();
+}
+
+
+const uint16_t GRAPH_SPACE = 2;
+
+void drawGraph(uint16_t data[], uint8_t cnt, uint8_t x, uint8_t y, uint16_t maxVal, uint16_t maxH) {
+  for (uint8_t i=0; i<cnt; i++) {
+    uint16_t rolls = maxVal < maxH ?
+      data[i] :
+      (((float)(data[i])) / ((float)maxVal)) * maxH; 
+
+    if (rolls == 0) {
+      rolls = 1;
+    }
+    display.fillRect(x+i*(GRAPH_SPACE+2), y - rolls, GRAPH_SPACE, rolls, BLACK);  
+  }
+}
+
+uint16_t getMaxRolls(uint16_t data[], uint8_t cnt) {
+  uint16_t maxRolls = 1;
+  for (uint8_t i=0; i<cnt; i++) {
+    if (data[i] > maxRolls) maxRolls = data[i];
+  }
+  return maxRolls;
+}
 
 
 
 
+void backLightOn() {
+  backLightOnTimer = millis();
+  digitalWrite(SCREEN_BACK_LIGHT, HIGH);
+}
 
-
-
+void checkBackLighthStatus() {
+  if ((millis() - backLightOnTimer) > LIGHT_ON_milliseconds) {
+    digitalWrite(SCREEN_BACK_LIGHT, LOW);
+  }  
+}
